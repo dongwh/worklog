4fc225c3be6f82338dd274d6d2800e46d8a74733
dongwh <dongwh@btte.net>
2018-09-29 10:26:41 +0800

    mybatis

--- worklog/src/main/java/com/drpeng/worklog/controller/LoginController.java
+++ worklog/src/main/java/com/drpeng/worklog/controller/LoginController.java
@@ -3,8 +3,6 @@ package com.drpeng.worklog.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
